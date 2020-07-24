ef7d7e2111d19cecb3299ce967fed40709b4b2cc
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-07-24 14:17:43 +0200

    Still can't get this right...

--- dns/.ci/run.sh
+++ dns/.ci/run.sh
@@ -3,7 +3,6 @@
 set -e
 
 pip install -r dns/requirements.txt
-ansible-playbook dns/tests/test.yml -i dns/tests/inventory --syntax-check
 ansible-lint dns/
 (cd dns && molecule lint)
 (cd dns && molecule test)
