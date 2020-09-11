07b2f6925715b754828bb87f05e550fc45417aa8
fhochleitner <Ka2Us6m1clvV>
2020-09-11 09:57:22 +0200

    fix things. da vagrant ein nogo ist für pxe mit raspbian und der raspi grad nicht zur verfügung steht, leider ungetestet
    Signed-off-by: fhochleitner <Ka2Us6m1clvV>

--- pxe-boot/files/client.regenerate_ssh_keys.sh
+++ pxe-boot/files/client.regenerate_ssh_keys.sh
@@ -1,6 +1,9 @@
-cd /nfs/client1
-sudo mount --bind /dev dev
-sudo mount --bind /sys sys
-sudo mount --bind /proc proc
-chroot /nfs/client1 'dpkg-reconfigure openssh-server'
-sudo umount dev sys proc
+#!/bin/bash
+directory=$1
+
+cd /nfs/${directory}
+echo "sudo mount --bind /dev dev"
+echo "sudo mount --bind /sys sys"
+echo "sudo mount --bind /proc proc"
+echo "chroot /nfs/${directory} 'dpkg-reconfigure openssh-server'"
+echo "sudo umount dev sys proc"
