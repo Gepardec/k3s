6c83c1df590ac38e8a3690b352dd113d2f230a26
fhochleitner <Ka2Us6m1clvV>
2020-09-18 12:45:49 +0200

    playbook plays without error. SHIP IT
    //todo: sinnvolle tags
    //test in prod
    
    Signed-off-by: fhochleitner <Ka2Us6m1clvV>

--- pxe-boot/files/client.regenerate_ssh_keys.sh
+++ pxe-boot/files/client.regenerate_ssh_keys.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-directory=$1
 
-cd /nfs/${directory}
+cd /nfs/client1
 echo "sudo mount --bind /dev dev"
 echo "sudo mount --bind /sys sys"
 echo "sudo mount --bind /proc proc"
-echo "chroot /nfs/${directory} 'dpkg-reconfigure openssh-server'"
+echo "chroot /nfs/client1 'dpkg-reconfigure openssh-server'"
 echo "sudo umount dev sys proc"
